0b25ce515eb8d00623b65d1478fbd69206a246e1
shahishikas <shahishikas@gmail.com>
2020-09-19 14:40:19 -0700

    set up basic timer

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
+let standInterval = 30; // in minutes
+let lastStand = new Date().getTime();
+let sinceStand = 0;     // time since last standing break, in minutes
+
+let updateStandTimer = setInterval(function() {
+    let now = new Date().getTime();
+    sinceStand = Math.round((now - lastStand) / (1000 * 60));
+    // TODO: update HTML
+    if (sinceStand >= standInterval) {
+        showStandReminder()
+    }
+}, 1000*60);
+
+function showStandReminder() {
+    // TODO: display the popup
+}
+
 function setStand() {
     /// idk yet bro
 }
