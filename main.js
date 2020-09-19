9975df3801132987479cc557ccc228afc386a185
luclaire03 <clairelu03@gmail.com>
2020-09-19 16:31:21 -0500

    reveal text on click

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 function setStand() {
     /// idk yet bro
+    document.getElementById('standtext').style.display = 'block';
+    document.getElementById('countdown').style.display = 'block';
 }
 
 function setSleep() {
     /// also dk yet
+    document.getElementById('sleeptext').style.display = 'block';
 }
